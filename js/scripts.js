46ac665c24a2e9ef82bca108d4e0b03e6fdac410
AoYz <34399520+AoYz@users.noreply.github.com>
2018-01-21 22:37:51 +0200

    Menu page, Contact page

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,26 @@
 $(document).ready(function() {
+	/* Parallax on Front Page */ 
 	$('#parallax-front').parallax({imageSrc: './images/parallax.jpg'});
 
-	$( ".cross" ).hide();
-	$( ".toggle-menu" ).hide();
-	$( ".hamburger" ).click(function() {
-		$( ".toggle-menu" ).slideToggle( "slow", function() {
-			$( ".hamburger" ).hide();
-			$( ".cross" ).show();
-		});
+	// Navigation Bar change on Scroll 
+	var $navbarHeight = $('.navbar').height();
+	$(window).scroll(function() {
+		var $scroll = $(window).scrollTop();
+		console.log($scroll);
+		if ($scroll >= $navbarHeight + 50) {
+			console.log('x');
+		  $(".navbar").addClass("navbar-scrolled");
+	  } else {
+	  	$('.navbar').removeClass("navbar-scrolled");
+	  }
 	});
 
-	$( ".cross" ).click(function() {
-		$( ".toggle-menu" ).slideToggle( "slow", function() {
-			$( ".cross" ).hide();
-			$( ".hamburger" ).show();
-		});
+	// Navbar on smaller devices
+
+	var $hamburger = $('.navbar-toggle');
+	var $navigation = $('.navbar');
+	$hamburger.click(function() {
+		$navigation.toggleClass('collapsed');
 	});
-
-// Scroll
-var $navbarHeight = $('.navbar').height();
-$(window).scroll(function() {
-	var $scroll = $(window).scrollTop();
-	console.log($scroll);
-	if ($scroll >= $navbarHeight + 50) {
-		console.log('x');
-	  $(".navbar").addClass("navbar-scrolled");
-  } else {
-  	$('.navbar').removeClass("navbar-scrolled");
-  }
-});
-
-
 });
 
